ccbd053f77023fb4a16b3a6ba3422511f4c27e92
unikuam <anshul17071996@gmail.com>
2020-04-24 12:24:26 +0530

    Change Function Name & Use Arrow Function

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -2,16 +2,14 @@ function curry(fn) {
   if (typeof fn !== 'function') {
     throw new TypeError(`Expected function, got ${typeof fn}, please try again with a function argument`);
   }
-  return function internalCurrying(...args) {
+  return function curriedFn(...args) {
     if (args.length < 1 && fn.length > 0) {
       throw new Error(`Expected at least ${fn.length} number of arguments but got ${args.length}`);
     }
     if (args.length >= fn.length) {
       return fn(...args);
     } else {
-      return function(...curryArgs) {
-        return internalCurrying(...args.concat(curryArgs));
-      }
+      return (...curryArgs) => curriedFn(...args.concat(curryArgs));
     }
   }
 }
