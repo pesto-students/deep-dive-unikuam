eb093b2648d786cba41d35310a0c7e37a5b3ce0b
prashant kumar singh <singh.prashantking@gmail.com>
2020-05-16 16:39:12 +0530

    create public folder for static files and static js to work with static files

---
+++ node-exercises/simple-server-node/static.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const rootPath = __dirname;
+
+const mimeTypes = {
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".gif": "image/gif",
+};
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(rootPath, "public", req.url);
+  if (req.url === "" || req.url === "/" || req.url.match("index.html$")) {
+    fs.readFile("./public/index.html", "utf-8", (err, html) => {
+      res.writeHead(200, "OK", { "Content-Type": "text/html" });
+      res.end(html);
+    });
+  } else if (req.url.match(".css$")) {
+    let cssPath = filePath;
+    let fileStream = fs.createReadStream(cssPath, "utf-8");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    fileStream.pipe(res);
+  } else if (req.url.match(".png$")) {
+    let imagePath = filePath;
+    let fileStream = fs.createReadStream(imagePath);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    fileStream.pipe(res);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("No page found");
+  }
+});
+
+server.listen(4000);
