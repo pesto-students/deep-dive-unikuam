ab2d906f0a0799a9eaf09e6cdbed59b445107db2
prashant kumar singh <singh.prashantking@gmail.com>
2020-05-16 17:21:42 +0530

    add static file method for json as well and update files

--- node-exercises/simple-server-node/static.js
+++ node-exercises/simple-server-node/static.js
@@ -3,16 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const rootPath = __dirname;
 
-const mimeTypes = {
-  ".html": "text/html",
-  ".js": "text/javascript",
-  ".css": "text/css",
-  ".json": "application/json",
-  ".png": "image/png",
-  ".jpg": "image/jpg",
-  ".gif": "image/gif",
-};
-
 const server = http.createServer((req, res) => {
   const filePath = path.join(rootPath, "public", req.url);
   if (req.url === "" || req.url === "/" || req.url.match("index.html$")) {
@@ -20,15 +10,30 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, "OK", { "Content-Type": "text/html" });
       res.end(html);
     });
+  } else if (req.url.match(".html$")) {
+    let htmlPath = filePath;
+    let fileStream = fs.createReadStream(htmlPath, "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fileStream.pipe(res);
   } else if (req.url.match(".css$")) {
     let cssPath = filePath;
     let fileStream = fs.createReadStream(cssPath, "utf-8");
     res.writeHead(200, { "Content-Type": "text/css" });
     fileStream.pipe(res);
+  } else if (req.url.match(".js$")) {
+    let jsPath = filePath;
+    let fileStream = fs.createReadStream(jsPath, "utf-8");
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    fileStream.pipe(res);
   } else if (req.url.match(".png$")) {
     let imagePath = filePath;
     let fileStream = fs.createReadStream(imagePath);
     res.writeHead(200, { "Content-Type": "image/png" });
+    fileStream.pipe(res);
+  } else if (req.url.match(".json$")) {
+    let jsonPath = filePath;
+    let fileStream = fs.createReadStream(jsonPath);
+    res.writeHead(200, { "Content-Type": "application/json" });
     fileStream.pipe(res);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
