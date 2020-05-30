91907d59d8a5c6db9907e4b585acc2b8084a7da9
Aswin <aswin.harikumar@yahoo.com>
2020-05-30 12:04:57 +0530

    setup todo-backend

---
+++ node-exercises/simple-odm-node/backend/server.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const dbConnectUrl = process.env.DB_CONNECT;
+
+
+MongoClient.connect(dbConnectUrl, { useNewUrlParser: true })
+  .then((client) => {
+    console.log('db is connected ');
+    const db = client.db('todo-app');
+    const todoListCollection = db.collection('todo-list');
+    // app.use()
+    // app.get(/* ... */)
+    // app.post(/* ... */)
+    // app.listen(/* ... */)
+  })
+  .catch((error) => console.error(error));
